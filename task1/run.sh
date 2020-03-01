3679dc465643853a89af8fb88f2894925404d511
Nick <zaitsev.ni@phystech.edu>
2020-03-01 12:20:16 +0300

    Last

--- task1/run.sh
+++ task1/run.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
-recursion () {
-	path=$1
-	for obj in `ls $path`
-	do
-		if [[ -d $obj ]]
-		then recursion $obj; fi;
-	done
-	for obj in `find $path -name "*.$ext"`
-	do 
-		cp $obj ${bac}/`basename obj`$count".$ext"
-	       	((count+=1)) 
-	done
-}
 #directory extention backup archive
 dir=$1; ext=$2; bac=$3; arc=$4
 if [[ ! -e $bac ]]
 then mkdir $bac; fi;
 count=0
-recursion $dir
+for obj in `find $dir -name "*.$ext"`
+do
+	cp $obj ${bac}/$count`basename $obj`
+       	((count+=1)) 
+done
 tar -zcf $arc $bac
 echo done
